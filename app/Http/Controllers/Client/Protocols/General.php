6bc67dca08d2a27b3fb5ca4d04fec0d114e3d8c8
lotusproxy <106042519+lotusproxy@users.noreply.github.com>
2023-06-04 05:00:06 +0000

    fix sub

--- app/Http/Controllers/Client/Protocols/General.php
+++ app/Http/Controllers/Client/Protocols/General.php
@@ -105,7 +105,8 @@ class General
         $query = http_build_query([
             'allowInsecure' => $server['allow_insecure'],
             'peer' => $server['server_name'],
-            'sni' => $server['server_name']
+            'sni' => $server['server_name'],
+            'fp' => 'randomized'
         ]);
         $uri = "trojan://{$password}@{$server['host']}:{$server['port']}?{$query}#{$name}";
         $uri .= "\r\n";
